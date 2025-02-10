4a901803740f41f2ff7c9cd6ed2d1abdc8308bb7
spacenavy90 <10876259+spacenavy90@users.noreply.github.com>
2025-02-09 20:44:59 -0600

    remove halo bsg plugs

--- app/resources/[plug]/page.tsx
+++ app/resources/[plug]/page.tsx
@@ -1,6 +1,6 @@
 import { FilteredTable } from '@/components/filtered-table'
 
-type Plug = 'sw' | 'halo' | 'bsg'
+type Plug = 'sw'
 
 export default function ResourcePage({ params }: { params: { plug: Plug } }) {
     return (
